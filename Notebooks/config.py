71543562c70d8865c1376d6aa32ba84e9e6917c7
Patricia ENGOME <engomepatricia15@gmail.com>
2025-03-12 02:41:50 +0100

    Initial commit

--- Notebooks/config.py
+++ Notebooks/config.py
@@ -7,7 +7,7 @@ from scipy.stats import chi2_contingency
 import scipy.stats as stats
 import statsmodels.api as sm
 from sklearn.metrics import roc_curve, auc, confusion_matrix, classification_report
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 from collections import Counter
